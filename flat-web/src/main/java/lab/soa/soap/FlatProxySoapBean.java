f0e6d220dff080cb4a464d60b53120472627e38b
stoneshik <strebilly@mail.ru>
2026-02-19 20:44:50 +0300

    https для принятия запросов soap

--- flat-web/src/main/java/lab/soa/soap/FlatProxySoapBean.java
+++ flat-web/src/main/java/lab/soa/soap/FlatProxySoapBean.java
@@ -1,13 +1,9 @@
 package lab.soa.soap;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import jakarta.ejb.EJB;
 import jakarta.ejb.Stateless;
 import jakarta.jws.WebService;
 import lab.soa.ejb.FlatProxyRemote;
-import lab.soa.ejb.RequestManager;
 import lab.soa.presentation.dto.responses.ProxyResponseDto;
 import lab.soa.soap.fault.ServiceFaultException;
 
@@ -17,8 +13,6 @@ import lab.soa.soap.fault.ServiceFaultException;
     serviceName = "FlatProxyService"
 )
 public class FlatProxySoapBean implements FlatProxySoap {
-    private static final Logger log = LoggerFactory.getLogger(RequestManager.class);
-
     @EJB(lookup = "java:global/flat-ejb-1.0.0/FlatProxyBean!lab.soa.ejb.FlatProxyRemote")
     private FlatProxyRemote proxyEjb;
 
@@ -29,7 +23,6 @@ public class FlatProxySoapBean implements FlatProxySoap {
     ) throws ServiceFaultException {
         try {
             ProxyResponseDto response = proxyEjb.proxyFindWithBalcony(priceType, balconyType);
-            log.info(String.format("findWithBalcony response from first: %s", response));
             return response;
         } catch (Exception e) {
             throw new ServiceFaultException("Internal server error");
@@ -50,7 +43,6 @@ public class FlatProxySoapBean implements FlatProxySoap {
                 page,
                 size
             );
-            log.info(String.format("getOrderedByTimeToMetro response from first: %s", response));
             return response;
         } catch (Exception e) {
             throw new ServiceFaultException("Internal server error");
