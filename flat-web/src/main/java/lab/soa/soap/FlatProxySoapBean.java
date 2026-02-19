c6a4a54b0f3647c08ad1228cb8e197f8af556493
stoneshik <strebilly@mail.ru>
2026-02-19 20:11:45 +0300

    рабочий вариант

--- flat-web/src/main/java/lab/soa/soap/FlatProxySoapBean.java
+++ flat-web/src/main/java/lab/soa/soap/FlatProxySoapBean.java
@@ -1,9 +1,13 @@
 package lab.soa.soap;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import jakarta.ejb.EJB;
 import jakarta.ejb.Stateless;
 import jakarta.jws.WebService;
 import lab.soa.ejb.FlatProxyRemote;
+import lab.soa.ejb.RequestManager;
 import lab.soa.presentation.dto.responses.ProxyResponseDto;
 import lab.soa.soap.fault.ServiceFaultException;
 
@@ -13,6 +17,8 @@ import lab.soa.soap.fault.ServiceFaultException;
     serviceName = "FlatProxyService"
 )
 public class FlatProxySoapBean implements FlatProxySoap {
+    private static final Logger log = LoggerFactory.getLogger(RequestManager.class);
+
     @EJB(lookup = "java:global/flat-ejb-1.0.0/FlatProxyBean!lab.soa.ejb.FlatProxyRemote")
     private FlatProxyRemote proxyEjb;
 
@@ -22,7 +28,9 @@ public class FlatProxySoapBean implements FlatProxySoap {
         String balconyType
     ) throws ServiceFaultException {
         try {
-            return proxyEjb.proxyFindWithBalcony(priceType, balconyType);
+            ProxyResponseDto response = proxyEjb.proxyFindWithBalcony(priceType, balconyType);
+            log.info(String.format("findWithBalcony response from first: %s", response));
+            return response;
         } catch (Exception e) {
             throw new ServiceFaultException("Internal server error");
         }
@@ -36,12 +44,14 @@ public class FlatProxySoapBean implements FlatProxySoap {
         Integer size
     ) throws ServiceFaultException {
         try {
-            return proxyEjb.proxyGetOrderedByTimeToMetro(
+            ProxyResponseDto response = proxyEjb.proxyGetOrderedByTimeToMetro(
                 transportType,
                 sortType,
                 page,
                 size
             );
+            log.info(String.format("getOrderedByTimeToMetro response from first: %s", response));
+            return response;
         } catch (Exception e) {
             throw new ServiceFaultException("Internal server error");
         }
